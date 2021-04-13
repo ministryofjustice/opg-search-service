5b9040422c45ccbe76fd5a967c1b7d4118dc457a
devkokov <dm.kokov@gmail.com>
2021-04-13 14:30:02 +0100

    SW-3276 ability to create an index and search indices

--- elasticsearch/client.go
+++ elasticsearch/client.go
@@ -1,6 +1,10 @@
 package elasticsearch
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
 	"io/ioutil"
@@ -18,45 +22,49 @@ type HTTPClient interface {
 
 type ClientInterface interface {
 	Index(i Indexable) *IndexResult
+	Search(requestBody map[string]interface{}, dataType Indexable) ([]string, error)
+	CreateIndex(i Indexable) (bool, error)
 }
 
 type Indexable interface {
 	Id() int64
 	IndexName() string
 	Json() string
+	IndexConfig() map[string]interface{}
 }
 
 type Client struct {
 	httpClient HTTPClient
 	logger     *log.Logger
+	domain     string
+	region     string
+	service    string
+	signer     *v4.Signer
 }
 
 func NewClient(httpClient HTTPClient, logger *log.Logger) (ClientInterface, error) {
-	return &Client{
+	client := Client{
 		httpClient: httpClient,
 		logger:     logger,
-	}, nil
+		domain:     os.Getenv("AWS_ELASTICSEARCH_ENDPOINT"),
+		region:     os.Getenv("AWS_REGION"),
+		service:    "es",
+		signer:     v4.NewSigner(credentials.NewEnvCredentials()),
+	}
+
+	if client.region == "" {
+		client.region = "eu-west-1"
+	}
+
+	return &client, nil
 }
 
 func (c Client) Index(i Indexable) *IndexResult {
 	c.logger.Printf("Indexing %T ID %d", i, i.Id())
 
-	// Basic information for the Amazon Elasticsearch Service domain
-	domain := os.Getenv("AWS_ELASTICSEARCH_ENDPOINT") // e.g. https://my-domain.region.es.amazonaws.com
-	endpoint := domain + "/" + i.IndexName() + "/_doc/" + strconv.FormatInt(i.Id(), 10)
-
-	var region string
-	var ok bool
-	if region, ok = os.LookupEnv("AWS_REGION"); !ok {
-		region = "eu-west-1"
-	}
-	service := "es"
+	endpoint := c.domain + "/" + i.IndexName() + "/_doc/" + strconv.FormatInt(i.Id(), 10)
 
 	body := strings.NewReader(i.Json())
-
-	// Get credentials from environment variables and create the AWS Signature Version 4 signer
-	cred := credentials.NewEnvCredentials()
-	signer := v4.NewSigner(cred)
 
 	iRes := IndexResult{Id: i.Id()}
 
@@ -73,7 +81,7 @@ func (c Client) Index(i Indexable) *IndexResult {
 	req.Header.Add("Content-Type", "application/json")
 
 	// Sign the request, send it, and print the response
-	_, _ = signer.Sign(req, body, service, region, time.Now())
+	_, _ = c.signer.Sign(req, body, c.service, c.region, time.Now())
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
@@ -96,4 +104,89 @@ func (c Client) Index(i Indexable) *IndexResult {
 	}
 
 	return &iRes
+}
+
+// returns an array of JSON encoded results
+func (c Client) Search(requestBody map[string]interface{}, i Indexable) ([]string, error) {
+	endpoint := c.domain + "/" + i.IndexName() + "/_search"
+
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(requestBody); err != nil {
+		return nil, err
+	}
+	body := bytes.NewReader(buf.Bytes())
+
+	// Form the HTTP request
+	req, err := http.NewRequest(http.MethodPost, endpoint, body)
+	if err != nil {
+		return nil, err
+	}
+
+	// You can probably infer Content-Type programmatically, but here, we just say that it's JSON
+	req.Header.Add("Content-Type", "application/json")
+
+	// Sign the request, send it, and print the response
+	_, _ = c.signer.Sign(req, body, c.service, c.region, time.Now())
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		buf.Reset()
+		_, _ = buf.ReadFrom(resp.Body)
+		return nil, errors.New(fmt.Sprintf("search request failed with status code %d and response: `%s`", resp.StatusCode, buf.String()))
+	}
+
+	var r map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+		return nil, errors.New(fmt.Sprintf("Error parsing the response body: %s", err))
+	}
+
+	var results []string
+	for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
+		buf.Reset()
+		_ = json.NewEncoder(&buf).Encode(hit.(map[string]interface{})["_source"])
+		results = append(results, buf.String())
+	}
+
+	return results, nil
+}
+
+func (c Client) CreateIndex(i Indexable) (bool, error) {
+	c.logger.Printf("Creating index '%s' for %T", i.IndexName(), i)
+
+	endpoint := c.domain + "/" + i.IndexName()
+
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(i.IndexConfig()); err != nil {
+		return false, err
+	}
+	body := bytes.NewReader(buf.Bytes())
+
+	// Form the HTTP request
+	req, err := http.NewRequest(http.MethodPut, endpoint, body)
+	if err != nil {
+		return false, err
+	}
+
+	// You can probably infer Content-Type programmatically, but here, we just say that it's JSON
+	req.Header.Add("Content-Type", "application/json")
+
+	// Sign the request, send it, and print the response
+	_, _ = c.signer.Sign(req, body, c.service, c.region, time.Now())
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		buf.Reset()
+		_, _ = buf.ReadFrom(resp.Body)
+		return false, errors.New(fmt.Sprintf("index creation failed with status code %d and response: `%s`", resp.StatusCode, buf.String()))
+	}
+
+	return true, nil
 }
