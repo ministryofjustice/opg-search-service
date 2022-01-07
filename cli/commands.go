8da21a261cb9eacc6f39a2e90170f10fe7b25c7a
Joshua Hawxwell <m@hawx.me>
2022-01-07 13:22:41 +0000

    Make commands act like subcommands
    Currently everything is a global flag, which means we wouldn't be able to
    introduce something else with -force, for example. This makes things callable
    as subcommands, which can then define their own flagset.
    
    To allow transitioning existing uses -hc and -create-indices can still be called
    the old way too.

--- cli/commands.go
+++ cli/commands.go
@@ -7,33 +7,45 @@ import (
 )
 
 type Command interface {
+	Name() string
 	DefineFlags()
 	ShouldRun() bool
-	Run()
+	Run(args []string) error
 }
 
-type commands struct {
-	logger *logrus.Logger
-}
-
-func Commands(logger *logrus.Logger) *commands {
-	c := commands{
-		logger,
-	}
-	return &c
-}
-
-func (c commands) Register(cmds ...Command) {
+func Run(logger *logrus.Logger, cmds ...Command) {
 	for _, cmd := range cmds {
 		cmd.DefineFlags()
 	}
 
 	flag.Parse()
+	args := flag.Args()
 
-	for _, cmd := range cmds {
-		if cmd.ShouldRun() {
-			c.logger.Printf("Running command: %T", cmd)
-			cmd.Run()
+	if len(args) > 0 {
+		for _, cmd := range cmds {
+			if cmd.Name() == args[0] {
+				logger.Printf("Running command: %T", cmd)
+				if err := cmd.Run(args[1:]); err != nil {
+					logger.Errorln(err)
+					logger.Exit(1)
+					return
+				}
+
+				logger.Exit(0)
+			}
+		}
+	} else {
+		for _, cmd := range cmds {
+			if cmd.ShouldRun() {
+				logger.Printf("Running command: %T", cmd)
+				if err := cmd.Run(args); err != nil {
+					logger.Errorln(err)
+					logger.Exit(1)
+					return
+				}
+
+				logger.Exit(0)
+			}
 		}
 	}
 }
--- cli/create_indices_test.go
+++ cli/create_indices_test.go
@@ -4,141 +4,98 @@ import (
 	"errors"
 	"opg-search-service/elasticsearch"
 	"opg-search-service/person"
-	"os"
 	"testing"
 
 	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCreateIndices(t *testing.T) {
-	l, _ := test.NewNullLogger()
-	ci := NewCreateIndices(l)
-	assert.IsType(t, new(createIndices), ci)
-	assert.IsType(t, new(elasticsearch.Client), ci.esClient)
-	assert.Nil(t, ci.shouldRun)
-	assert.IsType(t, os.Exit, ci.exit)
-	assert.Same(t, l, ci.logger)
-}
-
-func TestCreateIndices_DefineFlags(t *testing.T) {
-	l, _ := test.NewNullLogger()
-	ci := NewCreateIndices(l)
-	assert.Nil(t, ci.shouldRun)
-	ci.DefineFlags()
-	assert.False(t, *ci.shouldRun)
-}
-
-func TestCreateIndices_ShouldRun(t *testing.T) {
-	tests := []struct {
-		scenario  string
-		shouldRun bool
-	}{
-		{
-			scenario:  "Command should run",
-			shouldRun: true,
-		},
-		{
-			scenario:  "Command should not run",
-			shouldRun: false,
-		},
-	}
-	for _, test := range tests {
-		ci := &createIndices{
-			shouldRun: &test.shouldRun,
-		}
-		assert.Equal(t, test.shouldRun, ci.ShouldRun(), test.scenario)
-	}
-}
-
-func TestCreateIndices_Run(t *testing.T) {
-	const ESErrorMessage = "some ES error"
+func TestCreateIndicesRun(t *testing.T) {
+	const esErrorMessage = "some ES error"
 
 	tests := []struct {
-		scenario     string
-		force        bool
-		esError      error
-		esExists     bool
-		esExistsErr  error
-		esDeleteErr  error
-		wantInLog    []string
-		wantExitCode int
+		scenario    string
+		force       bool
+		esError     error
+		esExists    bool
+		esExistsErr error
+		esDeleteErr error
+		wantInLog   []string
+		wantErr     error
 	}{
 		{
-			scenario:     "Index created successfully",
-			esError:      nil,
-			esExists:     false,
-			esExistsErr:  nil,
-			wantInLog:    []string{"Person index created successfully"},
-			wantExitCode: 0,
+			scenario:    "Index created successfully",
+			esError:     nil,
+			esExists:    false,
+			esExistsErr: nil,
+			wantInLog:   []string{"Person index created successfully"},
+			wantErr:     nil,
 		},
 		{
-			scenario:     "Error when creating index",
-			esError:      errors.New(ESErrorMessage),
-			esExists:     false,
-			esExistsErr:  nil,
-			wantInLog:    []string{ESErrorMessage},
-			wantExitCode: 1,
+			scenario:    "Error when creating index",
+			esError:     errors.New(esErrorMessage),
+			esExists:    false,
+			esExistsErr: nil,
+			wantInLog:   []string{},
+			wantErr:     errors.New(esErrorMessage),
 		},
 		{
-			scenario:     "Index already exists",
-			esError:      nil,
-			esExists:     true,
-			esExistsErr:  nil,
-			wantInLog:    []string{"Person index already exists"},
-			wantExitCode: 0,
+			scenario:    "Index already exists",
+			esError:     nil,
+			esExists:    true,
+			esExistsErr: nil,
+			wantInLog:   []string{"Person index already exists"},
+			wantErr:     nil,
 		},
 		{
-			scenario:     "Error when checking if index exists",
-			esError:      nil,
-			esExists:     false,
-			esExistsErr:  errors.New(ESErrorMessage),
-			wantInLog:    []string{ESErrorMessage},
-			wantExitCode: 1,
+			scenario:    "Error when checking if index exists",
+			esError:     nil,
+			esExists:    false,
+			esExistsErr: errors.New(esErrorMessage),
+			wantInLog:   []string{},
+			wantErr:     errors.New(esErrorMessage),
 		},
 		{
-			scenario:     "Force delete existing index",
-			force:        true,
-			esError:      nil,
-			esExists:     true,
-			esExistsErr:  nil,
-			wantInLog:    []string{"Person index already exists"},
-			wantExitCode: 0,
+			scenario:    "Force delete existing index",
+			force:       true,
+			esError:     nil,
+			esExists:    true,
+			esExistsErr: nil,
+			wantInLog:   []string{"Person index already exists"},
+			wantErr:     nil,
 		},
 		{
-			scenario:     "Error deleting existing index",
-			force:        true,
-			esError:      nil,
-			esExists:     true,
-			esExistsErr:  nil,
-			esDeleteErr:  errors.New(ESErrorMessage),
-			wantInLog:    []string{"Person index already exists", "Changes are forced, deleting old index", ESErrorMessage},
-			wantExitCode: 1,
+			scenario:    "Error deleting existing index",
+			force:       true,
+			esError:     nil,
+			esExists:    true,
+			esExistsErr: nil,
+			esDeleteErr: errors.New(esErrorMessage),
+			wantInLog:   []string{"Person index already exists", "Changes are forced, deleting old index"},
+			wantErr:     errors.New(esErrorMessage),
 		},
 	}
 	for _, tc := range tests {
-		l, hook := test.NewNullLogger()
+		t.Run(tc.scenario, func(t *testing.T) {
+			l, hook := test.NewNullLogger()
 
-		esClient := new(elasticsearch.MockESClient)
-		esClient.On("IndexExists", person.Person{}).Times(1).Return(tc.esExists, tc.esExistsErr)
-		esClient.On("DeleteIndex", person.Person{}).Times(1).Return(tc.esDeleteErr)
-		esClient.On("CreateIndex", person.Person{}).Times(1).Return(tc.esError == nil, tc.esError)
+			esClient := new(elasticsearch.MockESClient)
+			esClient.On("IndexExists", person.Person{}).Times(1).Return(tc.esExists, tc.esExistsErr)
+			esClient.On("DeleteIndex", person.Person{}).Times(1).Return(tc.esDeleteErr)
+			esClient.On("CreateIndex", person.Person{}).Times(1).Return(tc.esError == nil, tc.esError)
 
-		exitCode := 666
-		ci := createIndices{
-			logger:   l,
-			esClient: esClient,
-			force:    &tc.force,
-			exit: func(code int) {
-				exitCode = code
-			},
-		}
+			ci := createIndices{
+				logger:   l,
+				esClient: esClient,
+				force:    &tc.force,
+			}
 
-		ci.Run()
+			err := ci.Run([]string{})
+			assert.Equal(t, tc.wantErr, err)
 
-		for i, message := range tc.wantInLog {
-			assert.Contains(t, hook.Entries[i].Message, message, tc.scenario)
-		}
-		assert.Equal(t, tc.wantExitCode, exitCode, tc.scenario)
+			for i, message := range tc.wantInLog {
+				assert.Contains(t, message, hook.Entries[i].Message)
+			}
+		})
 	}
 }
