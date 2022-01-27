e5a5edb9c92c7c1e1243e8b0a02de7b0b6a49288
Joshua Hawxwell <m@hawx.me>
2022-01-27 08:51:37 +0000

    Handle index changes
    Will create the new index, then index to the new and existing index. Commands
    can be used to change the index used for searching (via an alias) and cleanup
    old indices.

--- cli/create_indices.go
+++ cli/create_indices.go
@@ -2,40 +2,31 @@ package cli
 
 import (
 	"flag"
-	"net/http"
-	"opg-search-service/elasticsearch"
-
-	"github.com/sirupsen/logrus"
 )
 
 type IndexClient interface {
 	CreateIndex(name string, config []byte, force bool) error
 }
 
-type createIndices struct {
+type createIndicesCommand struct {
 	esClient    IndexClient
 	indexName   string
 	indexConfig []byte
 }
 
-func NewCreateIndices(logger *logrus.Logger, indexName string, indexConfig []byte) *createIndices {
-	esClient, err := elasticsearch.NewClient(&http.Client{}, logger)
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	return &createIndices{
+func NewCreateIndices(esClient IndexClient, indexName string, indexConfig []byte) *createIndicesCommand {
+	return &createIndicesCommand{
 		esClient:    esClient,
 		indexName:   indexName,
 		indexConfig: indexConfig,
 	}
 }
 
-func (c *createIndices) Name() string {
+func (c *createIndicesCommand) Name() string {
 	return "create-indices"
 }
 
-func (c *createIndices) Run(args []string) error {
+func (c *createIndicesCommand) Run(args []string) error {
 	flagset := flag.NewFlagSet("create-indices", flag.ExitOnError)
 
 	force := flagset.Bool("force", false, "force recreation if index already exists")
