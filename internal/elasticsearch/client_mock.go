adbe4de49212a2428baf1797ef06ece5449b20fd
GemTay <gemma.taylor@digital.justice.gov.uk>
2023-01-11 15:21:44 +0000

    now using http.NewRequestWithContext so when the parent function is cancelled, any inflight api requests will also be cancelled due to the cascading nature

--- internal/elasticsearch/client_mock.go
+++ internal/elasticsearch/client_mock.go
@@ -1,37 +1,40 @@
 package elasticsearch
 
-import "github.com/stretchr/testify/mock"
+import (
+	"context"
+	"github.com/stretchr/testify/mock"
+)
 
 type MockESClient struct {
 	mock.Mock
 }
 
-func (m *MockESClient) DoBulk(op *BulkOp) (BulkResult, error) {
-	args := m.Called(op)
+func (m *MockESClient) DoBulk(ctx context.Context, op *BulkOp) (BulkResult, error) {
+	args := m.Called(ctx, op)
 	return args.Get(0).(BulkResult), args.Error(1)
 }
 
-func (m *MockESClient) Search(indexName []string, requestBody map[string]interface{}) (*SearchResult, error) {
-	args := m.Called(indexName, requestBody)
+func (m *MockESClient) Search(ctx context.Context, indexName []string, requestBody map[string]interface{}) (*SearchResult, error) {
+	args := m.Called(ctx, indexName, requestBody)
 	return args.Get(0).(*SearchResult), args.Error(1)
 }
 
-func (m *MockESClient) Delete(indexName []string, requestBody map[string]interface{}) (*DeleteResult, error) {
-	args := m.Called(indexName, requestBody)
+func (m *MockESClient) Delete(ctx context.Context, indexName []string, requestBody map[string]interface{}) (*DeleteResult, error) {
+	args := m.Called(ctx, indexName, requestBody)
 	return args.Get(0).(*DeleteResult), args.Error(1)
 }
 
-func (m *MockESClient) CreateIndex(name string, config []byte, force bool) error {
-	args := m.Called(name, config, force)
+func (m *MockESClient) CreateIndex(ctx context.Context, name string, config []byte, force bool) error {
+	args := m.Called(ctx, name, config, force)
 	return args.Error(0)
 }
 
-func (m *MockESClient) ResolveAlias(alias string) (string, error) {
-	args := m.Called(alias)
+func (m *MockESClient) ResolveAlias(ctx context.Context, alias string) (string, error) {
+	args := m.Called(ctx, alias)
 	return args.String(0), args.Error(1)
 }
 
-func (m *MockESClient) CreateAlias(alias string, index string) error {
-	args := m.Called(alias, index)
+func (m *MockESClient) CreateAlias(ctx context.Context, alias string, index string) error {
+	args := m.Called(ctx, alias, index)
 	return args.Error(0)
 }
