18e0d8bd4525b7dde4a6923bdcf3811d4fbaf29f
ramchale <legionsofbob@gmail.com>
2023-03-21 15:12:05 +0000

    WIP

--- scripts/localstack/localstack_init.sh
+++ scripts/localstack/localstack_init.sh
@@ -4,10 +4,10 @@
 awslocal es create-elasticsearch-domain --domain-name opg
 
 # Set secrets in Secrets Manager
-awslocal secretsmanager create-secret --name local/jwt-key \
+awslocal secretsmanager create-secret --name /local/jwt-key \
    --description "JWT secret for Go services authentication" \
    --secret-string "MyTestSecret"
 
-awslocal secretsmanager create-secret --name local/user-hash-salt \
+awslocal secretsmanager create-secret --name /local/user-hash-salt \
    --description "Email salt for Go services authentication" \
    --secret-string "ufUvZWyqrCikO1HPcPfrz7qQ6ENV84p0"
