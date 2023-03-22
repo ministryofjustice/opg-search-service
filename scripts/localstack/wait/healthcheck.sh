8eae6809688b864c1002df16743f79c009b1211c
Richard McHale <legionsofbob@gmail.com>
2023-03-22 16:39:36 +0000

    Update localstack to 1.4 (#446)
    * Renovate Update localstack/localstack-full Docker tag to v1
    * Update localstack es address for consistency with Sirius
    * Use docker healthcheck for localstack
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

---
+++ scripts/localstack/wait/healthcheck.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Elasticsearch
+curl --silent http://localhost:4566/_localstack/health | grep "\"es\": \"running\"" || exit 1
+curl --write-out %{http_code} --silent --output /dev/null http://search-service.eu-west-1.es.localhost.localstack.cloud:4566/_cluster/health | grep 200 || exit 1
+awslocal es list-domain-names | grep '"search-service"' || exit 1
+
+curl -XPUT -H "Content-Type: application/json" http://search-service.eu-west-1.es.localhost.localstack.cloud:4566/_cluster/settings -d '{ "transient": { "cluster.routing.allocation.disk.threshold_enabled": false } }'
+echo "Disk thresholds disabled"
