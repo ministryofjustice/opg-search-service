ba4da529a0dbe4ff68135d0a7b2f35716b3e4d78
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-07-05 11:44:44 +0100

    Encode Postgres password
    So that it can support non-alphanumeric characters.
    
    Also remove the old terraform directory.
    
    FIxes VEGA-2571 #patch

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "service_name" {
-  value       = aws_ecs_service.search_service.name : ""
-  description = "Name of the service for use in task stabilizer"
-}
-
-output "task_count" {
-  value       = aws_ecs_service.search_service.desired_count : 0
-  description = "Number of tasks expected to be running in the ECS Service"
-}
