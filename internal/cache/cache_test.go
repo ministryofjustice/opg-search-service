3d7bb886f9fe77e11c81719074c6ccd8eda206ab
Sam Joseph <denialanderror@googlemail.com>
2025-07-31 12:28:16 +0100

    sfs-46: Upgrades to aws-sdk-go-v2

--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -2,8 +2,8 @@ package cache
 
 import (
 	"errors"
-	"github.com/aws/aws-sdk-go/service/secretsmanager"
-	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-secretsmanager-caching-go/v2/secretcache"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"os"
@@ -23,7 +23,7 @@ func TestNew(t *testing.T) {
 	oldEnv := os.Getenv("ENVIRONMENT")
 	_ = os.Setenv("ENVIRONMENT", "test_env")
 
-	sc := New()
+	sc := New(aws.NewConfig())
 	assert.IsType(t, new(SecretsCache), sc)
 	assert.Equal(t, "test_env", sc.env)
 	assert.IsType(t, new(secretcache.Cache), sc.cache)
@@ -73,72 +73,5 @@ func TestSecretsCache_GetSecretString(t *testing.T) {
 
 		assert.Equal(t, test.returnedSecret, secret, test.scenario)
 		assert.Equal(t, test.returnedErr, err, test.scenario)
-	}
-}
-
-func TestApplyAwsConfig(t *testing.T) {
-	tests := []struct {
-		scenario   string
-		endpoint   string
-		region     string
-		wantRegion string
-		role       string
-	}{
-		{
-			scenario:   "Blank AWS region",
-			endpoint:   "test-endpoint",
-			region:     "",
-			wantRegion: "eu-west-1",
-			role:       "test-role",
-		},
-		{
-			scenario:   "Custom AWS region",
-			endpoint:   "test-endpoint",
-			region:     "test-region",
-			wantRegion: "test-region",
-			role:       "test-role",
-		},
-		{
-			scenario:   "Blank Iam Role",
-			endpoint:   "test-endpoint",
-			region:     "test-region",
-			wantRegion: "test-region",
-			role:       "",
-		},
-	}
-	for _, test := range tests {
-		oldEndpoint := os.Getenv("AWS_SECRETS_MANAGER_ENDPOINT")
-		oldRegion := os.Getenv("AWS_REGION")
-		oldIamRole := os.Getenv("AWS_IAM_ROLE")
-
-		_ = os.Setenv("AWS_SECRETS_MANAGER_ENDPOINT", test.endpoint)
-		if test.region == "" {
-			_ = os.Unsetenv("AWS_REGION")
-		} else {
-			_ = os.Setenv("AWS_REGION", test.region)
-		}
-		if test.role == "" {
-			_ = os.Unsetenv("AWS_IAM_ROLE")
-		} else {
-			_ = os.Setenv("AWS_IAM_ROLE", test.role)
-		}
-
-		c := new(secretcache.Cache)
-
-		applyAwsConfig(c)
-
-		cl := c.Client.(*secretsmanager.SecretsManager)
-
-		assert.Equal(t, "https://"+test.endpoint, cl.Endpoint, test.scenario)
-		assert.Equal(t, test.wantRegion, *cl.Config.Region, test.scenario)
-
-		_ = os.Setenv("AWS_SECRETS_MANAGER_ENDPOINT", oldEndpoint)
-		_ = os.Setenv("AWS_REGION", oldRegion)
-
-		if oldIamRole == "" {
-			_ = os.Unsetenv("AWS_IAM_ROLE")
-		} else {
-			_ = os.Setenv("AWS_IAM_ROLE", oldIamRole)
-		}
 	}
 }
